4a0af63bb4181d7373e460a04e1794168b1a1b3b
Dohyeon Kim <xingchen8837@gmail.com>
2023-10-14 16:37:01 +0900

    11th commit

--- mealServiceDietInfo.py
+++ mealServiceDietInfo.py
@@ -1,17 +1,16 @@
+import requests, json
 from sensitiveInformations import *
 
+SCHUL_DEPT_CODE = 'J10'
+SCHUL_CODE = 7531103
+
 def getDietInfo(today):
-    import requests, json
-
-    schul_dept_code = 'J10'
-    schul_code = 7531103
-
     url = 'https://open.neis.go.kr/hub/mealServiceDietInfo' #사이트에 기재된 API 요청 주소
     parameters = {
-        'KEY' : mealServiceDietInfoApiKey,         #사이트에서 발급받은 API 인증키
+        'KEY' : DIET_INFO_API_KEY,         #사이트에서 발급받은 API 인증키
         'Type' : 'json',                                    #받아올 데이터의 자료 구조
-        'ATPT_OFCDC_SC_CODE' : schul_dept_code,         
-        'SD_SCHUL_CODE' : schul_code,               #각각에 대한 설명은 API 사이트에 설명되어 있다.
+        'ATPT_OFCDC_SC_CODE' : SCHUL_DEPT_CODE,         
+        'SD_SCHUL_CODE' : SCHUL_CODE,               #각각에 대한 설명은 API 사이트에 설명되어 있다.
         'MLSV_FROM_YMD' : today,
         'MLSV_TO_YMD' : today
     }
