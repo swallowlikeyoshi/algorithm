d2424f726f92ba5ddcc485c4060f25dd782ea5d1
Dohyeon Kim <xingchen8837@gmail.com>
2023-11-08 20:05:33 +0900

    20231108 2005

--- todaysUnyang/functions.py
+++ todaysUnyang/functions.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 from datetime import datetime
 from todaysUnyang import app
-import mealServiceDietInfo as meal
+from todaysUnyang import mealServiceDietInfo as meal
 
 @app.route('/')
 def index():
     return render_template('index.html', dietInfo = meal.getDietInfo(today()), calrorieInfo = meal.getCalorieInfo(today()))
 
-@app.route('/Developers')
-def get_developers_info():
+@app.route('/developers')
+def developers():
     return render_template('Developers.html')
+
+@app.route('/moreinfo')
+def moreinfo():
+    return render_template('Moreinfo.html')
+
+@app.route('/making')
+def making():
+    return render_template('Making.html')
 
 def today():
     return str(datetime.date(datetime.today())).replace('-', '')
--- todaysUnyang/mealServiceDietInfo.py
+++ todaysUnyang/mealServiceDietInfo.py
@@ -1,5 +1,5 @@
 import requests, json
-from sensitiveInformations import *
+from todaysUnyang import env
 
 SCHUL_DEPT_CODE = 'J10' #경기도교육청 코드
 SCHUL_CODE = 7531103 #운양고등학교 코드
@@ -33,7 +33,7 @@ def getNutritionInfo(today):
         return str(dietInfo['RESULT']['MESSAGE'])
 
 def _getMealServiceDietInfo(today):
-    with open('mealDietInfo.json', 'r', encoding='utf-8') as f:
+    with open('todaysUnyang/mealDietInfo.json', 'r', encoding='utf-8') as f:
         savedDietInfo = json.load(f)
     try:
         if (str(savedDietInfo['mealServiceDietInfo'][1]['row'][0]['MLSV_YMD']) == str(today)):
@@ -43,7 +43,7 @@ def _getMealServiceDietInfo(today):
     except:
         url = 'https://open.neis.go.kr/hub/mealServiceDietInfo' #사이트에 기재된 API 요청 주소
         parameters = {
-            'KEY' : DIET_INFO_API_KEY,                          #사이트에서 발급받은 API 인증키
+            'KEY' : env.DIET_INFO_API_KEY,                          #사이트에서 발급받은 API 인증키
             'Type' : 'json',                                    #받아올 데이터의 자료 구조
             'ATPT_OFCDC_SC_CODE' : SCHUL_DEPT_CODE,         
             'SD_SCHUL_CODE' : SCHUL_CODE,                       #각각에 대한 설명은 API 사이트에 설명되어 있다.
@@ -52,6 +52,6 @@ def _getMealServiceDietInfo(today):
         }
         requestsResponseData = requests.get(url, params=parameters)
         newDietInfo = json.loads(requestsResponseData.text)
-        with open('mealDietInfo.json', 'w') as f:
+        with open('todaysUnyang/mealDietInfo.json', 'w') as f:
             json.dump(newDietInfo, f, indent="\t")
         return newDietInfo
