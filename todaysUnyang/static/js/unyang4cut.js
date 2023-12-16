2e57f0a555b0a2759bf1bd835b8adaa69d9f54d5
Dohyeon Kim <xingchen8837@gmail.com>
2023-12-16 19:36:43 +0900

    download

--- todaysUnyang/static/js/unyang4cut.js
+++ todaysUnyang/static/js/unyang4cut.js
@@ -1,10 +1,10 @@
 let availableFrame = [false, false, false, false]
 let frame = 'black'
 let folderName = ''
-let imageNames = ['', '', '', '']
+let imageNames = [{}, {}, {}, {}]
 var imageFrameContainer = document.querySelectorAll('.imageFrameContainer')
 
-function pushImage(imageSrc) {
+function pushImage(imageSrc, imageAlt) {
     // framesDiv = document.getElementsByClassName('imageFrame')
     // framesDiv[imageIdx].src = imageSrc
     // framesDiv[imageIdx].classList.remove('disabled');
@@ -19,9 +19,11 @@ function pushImage(imageSrc) {
     }
     availableFrame[imageIdx] = true
     var image = imageFrameContainer[imageIdx].querySelector('.imageFrame')
-    var p = imageFrameContainer[imageIdx].querySelector('.designator')
+
     image.src = imageSrc
-    imageNames[imageIdx] = imageSrc
+    imageNames[imageIdx] = { url: image.src, fileName: imageAlt }
+
+    var p = imageFrameContainer[imageIdx].querySelector('.designator')
     image.classList.remove('disabled')
     p.classList.add('disabled')
 
@@ -53,5 +55,58 @@ function download() {
     collageImageSrc = collagedImage.src
     var downloadBtn = document.getElementById('downloadBtn')
     downloadBtn.href = collagedImage.src
+}
 
+function downloadFile(url, fileName) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+                const link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = fileName;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                resolve();
+            } else {
+                reject(new Error(`Failed to download file: ${xhr.status}`));
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(new Error('Network error occurred'));
+        };
+
+        xhr.send();
+    });
+}
+
+// 여러 파일을 동시에 다운로드하는 함수
+function downloadAllFiles() {
+    var files = [];
+    var takenImages = document.getElementsByClassName('img-fluid rounded mb-4 mb-lg-0 takenImages');
+
+    for (const takenImage of takenImages) {
+        files.push({ url: takenImage.src, fileName: takenImage.alt });
+    }
+
+    var collagedImage = document.getElementById('collagedImage')
+    files.push({ url: collagedImage.src, fileName: collagedImage.alt })
+
+    // Promise 배열을 생성하여 각 다운로드 작업을 시작
+    const downloadPromises = files.map(file => downloadFile(file.url, file.fileName));
+
+    // 모든 다운로드 작업이 완료될 때까지 대기
+    Promise.all(downloadPromises)
+        .then(() => {
+            console.log('All files downloaded successfully');
+        })
+        .catch(error => {
+            console.error(`Error downloading files: ${error.message}`);
+        });
 }
